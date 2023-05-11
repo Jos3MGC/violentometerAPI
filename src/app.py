490d8f3aafac052586e8898d4cbd0b030e68ae80
JOSÉ MARÍA GONZÁLEZ CRUZ <jmaria353@gmail.com>
2023-05-10 18:14:29 -0600

    .

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+from werkzeug.utils import secure_filename
+import io
 from flask_mysqldb import MySQL
 
 from config import config
@@ -7,7 +10,18 @@ app=Flask(__name__)
 
 connection = MySQL(app)
 
-
+@app.route('/send-file', methods=['POST'])
+def send_file():
+    url = 'https://colab.research.google.com/drive/1OXGILvSZSyabRr_KS6_6dLxdwqKzn2Gs?usp=sharing' # Replace with your own Colab URL
+    file = request.files['file']
+    if file.filename.endswith('.txt'):
+        filename = secure_filename(file.filename)
+        file_bytes = io.BytesIO(file.read())
+        files = {'file': (filename, file_bytes)}
+        response = requests.post(url, files=files)
+        return response.text
+    else:
+        return 'The attached file is not a .txt file'
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
