42846cff9dd9a96412d6a2d12ed17b0d6632b5d4
JOSÉ MARÍA GONZÁLEZ CRUZ <98995632+Jos3MGC@users.noreply.github.com>
2023-05-15 10:30:40 -0600

    .

--- src/app.py
+++ src/app.py
@@ -1,7 +1,4 @@
 from flask import Flask, request
-import requests
-from werkzeug.utils import secure_filename
-import io
 from flask_mysqldb import MySQL
 
 from config import config
@@ -10,18 +7,17 @@ app=Flask(__name__)
 
 connection = MySQL(app)
 
-@app.route('/send-file', methods=['POST'])
-def send_file():
-    url = 'https://colab.research.google.com/drive/1OXGILvSZSyabRr_KS6_6dLxdwqKzn2Gs?usp=sharing' # Replace with your own Colab URL
+@app.route('/upload', methods=['POST'])
+def upload_file():
     file = request.files['file']
-    if file.filename.endswith('.txt'):
-        filename = secure_filename(file.filename)
-        file_bytes = io.BytesIO(file.read())
-        files = {'file': (filename, file_bytes)}
-        response = requests.post(url, files=files)
-        return response.text
+    if file and file.filename.endswith('.txt'):
+        content = file.read().decode('utf-8')
+        # Realiza las operaciones que desees con el contenido del archivo
+        # ...
+
+        return 'Archivo recibido y procesado con éxito.'
     else:
-        return 'The attached file is not a .txt file'
+        return 'Error: archivo no válido.'
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
